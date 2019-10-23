e19fe8e468a9b918b679c161b92d4347fdac384d
binbin37 <chenbin37@sohu.com>
2019-10-23 21:39:41 +0800

    提交ex10-4更新

--- ex4/ex10-4.py
+++ ex4/ex10-4.py
@@ -1,6 +1,6 @@
 import math
 
-#赋值程序开关
+# 赋值程序开关
 key = 1
 
 # 采集信息的函数
@@ -10,14 +10,15 @@ def myinput():
         size = float(input('请输入项目大小：（1代表标准大小，请输入小数）'))
         number = None
         time = float(input('请输入工时数量：（请输入小数）'))
-        return size,number,time
+        return size, number, time
         # 这里返回的是一个元组
     elif choice == '2':
         size = float(input('请输入项目大小：（1代表标准大小，请输入小数）'))
         number = int(input('请输入人力数量：（请输入整数）'))
         time = None
-        return size,number,time
+        return size, number, time
         # 这里返回的数据是一个元组
+
 
 # 完成计算的函数
 def estimated(my_input):
@@ -28,20 +29,22 @@ def estimated(my_input):
     # 人力计算
     if (number == None) and (time != None):
         number = math.ceil(size * 80 / time)
-        print('项目大小为%.1f个标准项目，如果需要在%.1f个工时完成，则需要人力数量为：%d人' %(size,time,number))
+        print('项目大小为%.1f个标准项目，如果需要在%.1f个工时完成，则需要人力数量为：%d人' % (size, time, number))
     # 工时计算
     elif (number != None) and (time == None):
         time = size * 80 / number
-        print('项目大小为%.1f个标准项目，使用%d个人力完成，则需要工时数量为：%.1f个' %(size,number,time))
+        print('项目大小为%.1f个标准项目，使用%d个人力完成，则需要工时数量为：%.1f个' % (size, number, time))
 
-#增加循环判断选择函数
+
+# 增加循环判断选择函数
 
 def again():
-    #声明全局变量key
+    # 声明全局变量key
     global key
     a1 = input('要继续吗，请输入y继续，输入其它退出：')
     if a1 != 'y':
         key = 0
+
 
 # 主函数
 def main():
@@ -49,6 +52,7 @@ def main():
         my_input = myinput()
         estimated(my_input)
         again()
+
 
 # 调用主函数
 main()
